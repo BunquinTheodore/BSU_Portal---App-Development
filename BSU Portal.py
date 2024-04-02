4ffdf46c4debd3524f9a269746646551de79918f
TheodoreBunquin <23-03102@g.batstate-u.edu.ph>
2024-04-02 23:30:15 +0800

    Update BSU Portal.py

--- BSU Portal.py
+++ BSU Portal.py
@@ -1,4 +1,4 @@
-#BSU Portal 
+#Batangas State University Portal 
 
 student_account = {}
 
@@ -15,7 +15,9 @@ def main():
                 password = input ("Enter password: ")
                 sign_up(sr_code,password)
             elif choice == 2:
-                sign_in()
+                sr_code = input ("Enter SR Code: ")
+                password = input ("Enter password: ")
+                sign_in(sr_code, password)
             elif choice == 3:
                 print ("Exiting Portal...")
                 break
@@ -31,8 +33,54 @@ def sign_up(sr_code, password):
         student_account[sr_code] = {"password": password}
         print ('Account Registered Successfully!')
 
-def sign_in():
+def sign_in(sr_code, password):
+    if sr_code in student_account:
+        if password == student_account[sr_code]["password"]:
+            sub_portal()
+        else: 
+            print ("wrong password!")
+    else:
+        print ("Student Account not Found!")
+        return
+            
+            
+def sub_portal():
+    while True:
+        print ("1. View Subjects")
+        print ("2. View ID")
+        print ("3. Schedules")
+        print ("4. Liabilities")
+        print ("5. Membership Payments")
+        print ("6. Curriculum")
+        print ("7. Scholarships")
+        print ("8. Online Registration")
+        print ("9. Certificate of Registration")
+
+def subjects():
     pass
 
+def curriculum():
+    pass
+
+def liabilities():
+    pass
+
+def schedules():
+    pass
+
+def membership_payments():
+    pass
+
+def view_ID():
+    pass
+
+def certificate_of_registration():
+    pass
+
+def scholarships():
+    pass
+
+def course_registration():
+    pass
 
 main()
