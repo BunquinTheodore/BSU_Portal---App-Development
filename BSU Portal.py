a3eb3a65d66e2eecceb27e12930072dc622a7ee9
TheodoreBunquin <23-03102@g.batstate-u.edu.ph>
2024-04-03 11:13:56 +0800

    Update BSU Portal.py

--- BSU Portal.py
+++ BSU Portal.py
@@ -1,6 +1,10 @@
 #Batangas State University Portal 
 
 student_account = {}
+coe_subjects = []
+cics_subjects = []
+cafad_subjects = []
+cit_subjects = []
 
 def main():
     while True: 
@@ -36,15 +40,61 @@ def sign_up(sr_code, password):
 def sign_in(sr_code, password):
     if sr_code in student_account:
         if password == student_account[sr_code]["password"]:
-            sub_portal()
+            year_level()
         else: 
             print ("wrong password!")
     else:
         print ("Student Account not Found!")
         return
             
+def year_level():
+    while True:
+        print ('1. 1st year')
+        print ('2. 2nd Year')
+        print ('3. 3rd Year')
+        print ('4. 4th Year')
+        choice = int(input('Enter Year Level: '))
+        if choice == 1:
+            school_department()
+            break
+        elif choice == 2:
+            school_department()
+            break
+        elif choice == 3:
+            school_department()
+            break
+        elif choice == 4:
+            school_department()
+            break
+        else: 
+            print ("Invalid Input!")
+            break
+        
+
+def school_department():
+    while True:
+        print ("1. College of Informatics and Computing Sciences")
+        print ("2. College of Engineering")
+        print ("3. College of Information Technology")
+        print ("4. College of Architecture, Fine Arts and Design")
+        choice = int(input("Enter College Department: "))
+        if choice == 1:
+            sub_portal(choice)
+            break
+        elif choice == 2:
+            sub_portal(choice)
+            break
+        elif choice == 3:
+            sub_portal(choice)
+            break
+        elif choice == 4:
+            sub_portal(choice)
+            break
+        else:
+            print ("Invalid Input!")
+            break
             
-def sub_portal():
+def sub_portal(choice):
     while True:
         print ("1. View Subjects")
         print ("2. View ID")
@@ -58,7 +108,7 @@ def sub_portal():
         choice = int(input("Enter choice: "))
         try:
             if choice == 1:
-                view_subjects()
+                view_subjects(choice)
             elif choice == 2:
                 view_ID()
             elif choice == 3:
@@ -80,8 +130,21 @@ def sub_portal():
         except ValueError as e:
             print (f"Error occured: {e}")
 
-def view_subjects():
-    pass
+def view_subjects(choice):
+    if choice == 1:
+        for subjects in cics_subjects:
+            print (subjects)
+    elif choice == 2:
+        for subjects in coe_subjects:
+            print (subjects)
+    elif choice == 3:
+        for subjects in cit_subjects:
+            print (subjects)
+    elif choice == 4:
+        for subjects in cafad_subjects:
+            print (subjects)
+    else: 
+        print ("Invalid Input!")
 
 def curriculum():
     pass
